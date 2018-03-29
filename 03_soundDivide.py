5045a8c49e3374011a3344c24bb24aceb4cd463c
simplecomplexity <cmplx.adpt.sys@gmail.com>
2018-03-30 00:30:24 +0900

    音分割プログラム

---
+++ 03_soundDivide.py
@@ -0,0 +1,44 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+# MORIUMIUS スペシャルサイエンスプログラム　音声ファイル分割
+# Sound Spectrogram
+# バージョン 1.0  Edited by Jun Komachi(2018/3/30)
+# matplotlib等のライブラリはできるだけ最新のバージョンを使ってください
+# pip install -U matplotlib でバージョンアップ可能です
+# 参考ホームページ
+# http://chachay.hatenablog.com/entry/2016/10/03/215841
+
+import os
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import matplotlib.pyplot as plt
+import argparse
+
+opt = argparse.ArgumentParser(description='Sound Spectrogram')
+opt.add_argument('-f', action='store', dest='file', required=True,
+                 help='Input audio file')
+opts = opt.parse_args()
+base, ext = os.path.splitext(opts.file)
+ext = ext.split('.')
+#print 'extention='+str(ext)
+sound = AudioSegment.from_file(opts.file, ext[1])
+
+chunks = split_on_silence(
+    sound,
+
+    # 1500ms以上の無音がある場所で分割
+    min_silence_len = 1500,
+
+    # -30dBFS以下で無音とみなす
+    silence_thresh  = -30,
+
+    # 分割後500msだけ無音を残す
+    keep_silence    = 500
+)
+
+# 分割数の表示
+print 'number of silence divide: ' + str(len(chunks))
+
+# ファイルの保存
+for i, chunk in enumerate(chunks):
+  chunk.export(base+'_'+str(i)+'.'+str(ext[1]), format=ext[1])
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # sound program in MORIUMIUS
+
+echo "# sound program in MORIUMIUS" >> README.md
+git init
+git add README.md 01_soundSpectrogram.py 02_heatmap.py 
+git commit -a -m 'First commit'
+git remote add origin https://github.com/simplecomplexity/sound.git
+git push -u origin master
+
+Username for 'https://github.com': simplecomplexity
+Password for 'https://simplecomplexity@github.com': 
+Counting objects: 5, done.
+Delta compression using up to 4 threads.
+Compressing objects: 100% (4/4), done.
+Writing objects: 100% (5/5), 2.44 KiB | 0 bytes/s, done.
+Total 5 (delta 0), reused 0 (delta 0)
+To https://github.com/simplecomplexity/sound.git
+ * [new branch]      master -> master
+Branch master set up to track remote branch master from origin.
